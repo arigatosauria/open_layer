dbdcb3204c07b3988402ab0f7fcd8c8eea3d92ce
arigatosauria <ari.gato.sauria@gmail.com>
2024-02-19 22:25:04 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,30 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [-99.12766, 19.42847],
+    projection: "EPSG:4326",
+    zoom: 11
+  })
+});
+
+document.getElementById('zoom-out').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom - 1);
+};
+
+document.getElementById('zoom-in').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom + 1);
+};
